9156288c9d1de3f464252f32794bd99137be84c6
najoan <tiaaa0012@gmail.com>
2023-11-07 15:53:57 +0900

    Cleaned

--- src/main/java/com/hyfata/installer/utils/HorizontalLine.java
+++ src/main/java/com/hyfata/installer/utils/HorizontalLine.java
@@ -8,11 +8,8 @@ public class HorizontalLine extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        int panelWidth = getWidth(); // JPanel의 가로 길이
+        int panelWidth = getWidth();
         int panelHeight = getHeight();
-
-        // 가로 줄 그리기
-        //g.drawLine(0, 0, panelWidth, 0);
         g.fillRect(0, 0, panelWidth, panelHeight);
     }
 }
